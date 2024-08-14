122e8ceafa7c0978e2e6994b3319da810fcaf55e
Yghor Gabriel <yghor.ag@hotmail.com>
2024-08-14 20:52:20 -0300

    Started Details page and added global theme and settings

--- RocketFront/src/main.jsx
+++ RocketFront/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Details } from "./pages/Details";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ RocketFront/src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_ROSE: "#FF859B",
+    BACKGROUND_BLACK: "#0D0C0F",
+
+    WHITE: "#F4EDE8",
+    ROSE_GOLD: "#FF859B",
+
+    GRAY_100: "#948F99",
+    GRAY_300: "#999591",
+    GRAY_500: "#3E3B47",
+  },
+};
